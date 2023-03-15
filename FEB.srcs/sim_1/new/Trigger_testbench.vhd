49bc75a5ece10229b86eee194649641b174e0187
mrigatti <103663080+mrigatti@users.noreply.github.com>
2023-03-15 14:25:24 -0500

    Add files via upload

--- FEB.srcs/sim_1/new/Trigger_testbench.vhd
+++ FEB.srcs/sim_1/new/Trigger_testbench.vhd
@@ -1,9 +1,7 @@
-library ieee;
+LIBRARY ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-library unisim;
-use unisim.vcomponents.all;
+use ieee.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
 
 use work.Proj_Def.all;
 
@@ -23,6 +21,7 @@ component Trigger is
 	BeamOn 				: buffer std_logic;
 	uBunch   			: buffer std_logic_vector(31 downto 0);
 	uBunchWrt			: out std_logic;
+	GPO			 		: inout std_logic;
 -- Microcontroller strobes
 	CpldRst				: in std_logic;
 	CpldCS				: in std_logic;
@@ -30,17 +29,19 @@ component Trigger is
 	uCWr 				: in std_logic;
 -- Microcontroller data and address buses	
 	uCA 				: in std_logic_vector(11 downto 0);
-	uCD 				: inout std_logic_vector(15 downto 0);
+	uCD 				: in std_logic_vector(15 downto 0);
 -- Geographic address pins
 	GA 					: in std_logic_vector(1 downto 0);
 -- Synchronous edge detectors of uC read and write strobes
 	uWRDL 				: in std_logic_vector(1 downto 0);
+	WRDL 				: in std_logic_vector(1 downto 0);
 -- LED/Flash Gate select line
 	PulseSel 			: buffer std_logic;
 -- LED pulser/Flash Gate
 	Pulse 				: out std_logic;
 	LEDSrc				: buffer std_logic;
-	GPI0 				: in std_logic
+	GPI0 				: in std_logic;
+	iCD				  	: inout std_logic_vector(15 downto 0)
 	);
 end component;
 
@@ -67,8 +68,13 @@ component AFE_DataPath is
     din_AFE0			: in Array_8x14; 
     din_AFE1			: in Array_8x14;
     done				: in std_logic_vector(1 downto 0); -- status of automatic alignment FSM
+	SerdesRst			: out std_logic_vector(1 downto 0);
 -- Pipeline signals
 	PipelineSet 		: in std_logic_vector (7 downto 0);	
+-- Histogram signals
+	Diff_Reg			: inout Arrays_8x2x14;
+	GateWidth	    	: inout Array_2x12;
+	GateReq 			: inout std_logic_vector (1 downto 0);
 -- Microcontroller strobes
 	CpldRst				: in std_logic;
 	CpldCS				: in std_logic;
@@ -82,29 +88,39 @@ component AFE_DataPath is
 	);
 end component;
 
+constant Clk100MHz_period: time := 10.0ns;  -- 100 MHz
 constant Clk200MHz_period: time := 5.0ns;   -- 200 MHz
 constant Clk160MHz_period: time := 6.25ns;  -- 160 MHz
 constant Clk80MHz_period:  time := 12.5ns;  -- 80 MHz
 constant Clk560MHz_period: time := 1.786ns; -- 80 MHz * 7 = 560MHz
+constant Clk10MHz_period: time := 100ns;
+constant Clk12MHz_period: time := 83ns;
+constant Clk15MHz_period: time := 67ns;
 
 signal reset	: std_logic := '1';
 signal ResetHi	: std_logic := '1';
 signal CpldRst	: std_logic := '1';
-signal Clk200MHz, Clk160MHz, Clk80MHz, Clk560MHz: std_logic := '0';
+signal Clk100MHz, Clk200MHz, Clk160MHz, Clk80MHz, Clk560MHz: std_logic := '0';
 
-
+-- Synchronous edge detectors of uC read and write strobes
+signal WRDL 	: std_logic_vector(1 downto 0):=(others => '0');
+signal RDDL 				  : std_logic_vector(1 downto 0):=(others => '0');
+signal uWRDL 				  : std_logic_vector(1 downto 0):=(others => '0');
+signal uRDDL 				  : std_logic_vector(1 downto 0):=(others => '0');
+signal AddrReg				  : std_logic_vector(11 downto 0):=(others => '0');
+signal uAddrReg				  : std_logic_vector(11 downto 0):=(others => '0');
+	
 signal dout_AFE0		      : Array_8x14; 
 signal dout_AFE1		      : Array_8x14; 
 signal done					  : std_logic_vector(1 downto 0);
 
-
-signal CpldCS 				  : std_logic;	
-signal uCRd	  				  : std_logic;
-signal uCWr 				  : std_logic;		
+signal CpldCS 				  : std_logic := '0';
+signal uCRd	  				  : std_logic := '0';
+signal uCWr 				  : std_logic := '0';
 signal uCA 	  				  : std_logic_vector(11 downto 0);
 signal uCD 	  				  : std_logic_vector(15 downto 0);
 signal GA 					  : std_logic_vector(1 downto 0);
-signal uWRDL 				  : std_logic_vector(1 downto 0);
+signal GPO					  : std_logic;
 signal TrigReq				  : std_logic;
 signal PipelineSet 			  : std_logic_vector (7 downto 0) := X"04";
 signal BeamOn				  : std_logic;
@@ -123,6 +139,11 @@ signal ADCSmplCntReg 		  : std_logic_vector (3 downto 0);
 signal SlfTrgEn 			  : std_logic;
 signal uBunch   			  : std_logic_vector(31 downto 0);
 signal uBunchWrt			  : std_logic;
+-- Histogram signals from AFE_DataPath
+signal Diff_Reg				  : Arrays_8x2x14;
+signal GateWidth	    	  : Array_2x12;
+signal GateReq 				  : std_logic_vector (1 downto 0);
+
 
 
 signal d0_vec: std_logic_vector(13 downto 0) := "00100000000001"; -- 0x801
@@ -141,19 +162,58 @@ begin
 reset 	<= '1', '0' after 13ns;
 CpldRst <= '1', '0' after 12.5ns, '1' after 25ns;
 ResetHi <= not CpldRst;
-uCWR	<= '0';
-CpldCS	<= '0';
 
 
 -- make the clocks
+Clk100MHz  <= not Clk100MHz  after Clk100MHz_period/2;
 Clk80MHz  <= not Clk80MHz  after Clk80MHz_period/2;
 Clk160MHz <= not Clk160MHz after Clk160MHz_period/2;
 Clk200MHz <= not Clk200MHz after Clk200MHz_period/2;
 Clk560MHz <= not Clk560MHz after Clk560MHz_period/2;
+	
+--uCRd	<= not uCRd	after Clk10MHz_period/2;
+--uCWr	<= not uCWr	after Clk12MHz_period/2;
+--CpldCS	<= not CpldCS after Clk15MHz_period/2;
+--done <= "00", "11" after 50ns;
+--GA <= "00";
 
-done <= "00", "11" after 50ns;
-
-GA <= "00";
+global_signals_160MHz : process(Clk160MHz, CpldRst)
+	begin 
+	if CpldRst = '0' then
+	WRDL 	<= "00";
+	RDDL 	<= "00";
+	AddrReg <= (others => '0'); 
+	elsif rising_edge (Clk160MHz) then
+	-- Synchronous edge detectors for read and write strobes
+	WRDL(0) <= not uCWR and not CpldCS;
+	WRDL(1) <= WRDL(0);
+	RDDL(0) <= not uCRD and not CpldCS;
+	RDDL(1) <= RDDL(0);
+		if RDDL = 1 or WRDL = 1 
+		then AddrReg <= uCA;
+		else AddrReg <= AddrReg;
+		end if;
+	end if;
+end process;
+	
+global_signals_100MHz : process(Clk100MHz, CpldRst)
+	begin 
+	if CpldRst = '0' then
+	uWRDL 	<= "00"; 
+	uRDDL 	<= "00";
+	uAddrReg <= (others => '0');
+	elsif rising_edge (Clk100MHz) then
+	-- Synchronous edge detectors for read and write strobes
+	uWRDL(0) <= not uCWR and not CpldCS;
+	uWRDL(1) <= uWRDL(0);	
+	uRDDL(0) <= not uCRD and not CpldCS;
+	uRDDL(1) <= uRDDL(0);
+		if uRDDL = 1 or uWRDL = 1 
+		then uAddrReg <= uCA;
+		else uAddrReg <= uAddrReg;
+		end if;
+	end if;
+end process;
 
 sender: process
 begin
@@ -186,69 +246,88 @@ begin
 	d0_vec <= std_logic_vector(unsigned(d0_vec)+1);	
 end process sender;
 
+
 AFE_DataPath_inst : AFE_DataPath
 port map (
 	Clk_80MHz	    => Clk80MHz,		
 	SysClk			=> Clk160MHz,
 	ResetHi			=> ResetHi,
+-- Signals from Trigger Logic
 	TrigReq			=> TrigReq,
 	BeamOn			=> BeamOn,
-	MaskReg			=> MaskReg,					
-	BufferRdAdd		=> BufferRdAdd,				
-	BufferOut 		=> BufferOut, 
+-- Signals for EventBuilder
+	MaskReg			=> MaskReg,			
+	BufferRdAdd		=> BufferRdAdd,		
+	BufferOut 		=> BufferOut, 		
+-- Signals from uC
 	ControllerNo 	=> ControllerNo, 
-	PortNo 		    => PortNo, 		
-	BeamOnLength    => BeamOnLength, 
-	BeamOffLength   => BeamOffLength,
-	ADCSmplCntReg	=> ADCSmplCntReg,
+	PortNo 			=> PortNo,	
+	BeamOnLength    => BeamOnLength,
+	BeamOffLength   => BeamOffLength, 
+	ADCSmplCntReg   => ADCSmplCntReg,
+-- Data output from the deserializer for AFE0 and AFE1 synchronized to 80 MHz clock
     din_AFE0		=> dout_AFE0,
     din_AFE1		=> dout_AFE1,
 	done 			=> done,
+-- Pipeline signals 	
 	PipelineSet		=> PipelineSet,
-
+-- Histogram signals
+	Diff_Reg		=> Diff_Reg,	
+	GateWidth	    => GateWidth,
+	GateReq 		=> GateReq, 	
+-- Microcontroller strobes
 	CpldRst			=> CpldRst,	
 	CpldCS			=> CpldCS,
 	uCRd			=> uCRd,
 	uCWr 			=> uCWr, 	
+-- Microcontroller data and address buses	
 	uCA				=> uCA,
 	uCD             => uCD,
+-- Geographic address pins
 	GA              => GA 
- 			    
-	);
+);
 
 Trigger_logic: Trigger 
 port map(
   	SysClk			=> Clk160MHz, -- 160 MHz
 	ResetHi  		=> ResetHi,
+--Signals to AFE_DataPath
 	TrigReq			=> TrigReq,
-	SlfTrgEn 		=> SlfTrgEn, 
-	BeamOn 	        => BeamOn, 	
-	uBunch          => uBunch,   
-	uBunchWrt       => uBunchWrt,
+	BeamOn			=> BeamOn,
+-- Signals to EVB and DDR
+	uBunch   		=> uBunch,   
+	uBunchWrt		=> uBunchWrt,
+	SlfTrgEn		=> SlfTrgEn,
+-- Signal to Phase_Detector
+	GPO				=> GPO,		
+-- Microcontroller strobes
 	CpldRst			=> CpldRst,	
-	CpldCS			=> CpldCS,
+	CpldCS			=> CpldCS,	
 	uCRd			=> uCRd,
 	uCWr 			=> uCWr, 	
+-- Microcontroller data and address buses	
 	uCA 			=> uCA,
 	uCD 			=> uCD,
-	GA 				=> GA, 
+-- Geographic address pins
+	GA 				=> GA,
+-- Global signals
 	uWRDL 			=> uWRDL,
-	PulseSel 		=> PulseSel,
+	WRDL 			=> WRDL,
+-- Chip out
+	PulseSel 		=> PulseSel, 
 	Pulse 			=> Pulse, 
-	LEDSrc 			=> LEDSrc,	
-	GPI0 			=> GPI0 
+	GPI0 			=> GPI0,
+-- uC register 
+	LEDSrc 			=> LEDSrc
+
 );
 
-trigger_request: process
-begin
-	wait until rising_edge(Clk160MHz);
-	uWRDL 			  <= "01";
-	uCA(11 downto 10) <= "00";
-	uCA(9 downto 0)   <= FlashCtrlAddr;	
-	uCA(9 downto 0)   <= IntTrgEnAddr after Clk80MHz_period*4;
-
-end process;
-
-
+CpldCS <= '0';
+uWRDL 	<= "00";
+uCA(11 downto 10) <= "00";
+uCA(9 downto 0) <= FlashCtrlAddr;
+uCD <= X"ABCD";   
+--uCD <= (others => 'Z'); 
+  
 end Trigger_testbench_arch;
 
